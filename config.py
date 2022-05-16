49e3416e0f1b1886b785525b03bb2e4abfbdb37e
alibahramiyan <ali.bahramiyan@snapp.cab>
2022-05-16 19:38:58 +0430

    files

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
+from distutils.debug import DEBUG
+from email.policy import default
 from decouple import config
+
+DATABASE_URI = config('DATABASE_URI')
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = config('SECRET_KEY', default='guess-me')
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class stagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+
